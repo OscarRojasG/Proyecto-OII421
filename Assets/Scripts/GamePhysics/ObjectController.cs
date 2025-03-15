72cbadea2df090f9c894ea3218b92ab617ae013b
Oscar <oscar.rojas.g@mail.pucv.cl>
2025-03-15 16:02:00 -0300

    destroy object when it leaves the screen

--- Assets/Scripts/GamePhysics/ObjectController.cs
+++ Assets/Scripts/GamePhysics/ObjectController.cs
@@ -5,9 +5,21 @@ public class ObjectController : MonoBehaviour
     public float speedX = 2f; // Velocidad constante
     private Rigidbody2D rb;
 
+    private float startPos;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.linearVelocityX = speedX; // Mantiene la velocidad constante
+
+        startPos = transform.position.x;
+    }
+
+    private void Update()
+    {
+        if (transform.position.x <= -startPos)
+        {
+            Destroy(gameObject);
+        }
     }
 }
