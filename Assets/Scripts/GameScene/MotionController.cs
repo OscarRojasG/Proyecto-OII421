4f0acfaa1830d53af8cc3bcf5b21f73fe2cb3a83
Oscar <oscar.rojas.g@mail.pucv.cl>
2025-03-21 19:17:15 -0300

    add motion to background

---
+++ Assets/Scripts/GameScene/MotionController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System;
+
+public class MotionController : MonoBehaviour
+{
+    public float speed = -5f;
+
+    private float cloneStartX;
+    private bool cloned = false;
+
+    void Start()
+    {
+        Tilemap tilemap = GetComponentInChildren<Tilemap>();
+        tilemap.CompressBounds(); // Eliminar tiles vacíos
+
+        float width = tilemap.cellBounds.size.x * tilemap.layoutGrid.cellSize.x * transform.localScale.x;
+        cloneStartX = width;
+    }
+
+    void Update()
+    {
+        transform.localPosition += speed * Time.deltaTime * Vector3.right;
+
+        if (transform.localPosition.x <= 0 && cloned == false)
+        {
+            GameObject clone = Instantiate(gameObject, transform.parent);
+            clone.name = gameObject.name;
+            clone.transform.localPosition = new Vector3((float) Math.Floor(transform.localPosition.x) + cloneStartX, 0, 0);
+            cloned = true;
+        }
+        if (transform.localPosition.x <= -cloneStartX)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
